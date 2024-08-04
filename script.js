39b41c8415c7357ae1ce274d5c5140af8163e80c
VonMint <51849299+VonMint@users.noreply.github.com>
2024-08-04 15:01:31 +0200

    Create first implementation of game logic

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ function getComputerChoice(){
         return "ROCK";
     }else if (randomNumber < 67 && randomNumber > 33){
         return "PAPER";
-    }else{
-        return "SCISSORS";
-    }
+    }else return "SCISSORS";
+    
 }
 
 //Complementary function for getComputerChoice,
@@ -30,4 +29,45 @@ function getPlayerChoice(){
 
 function trimAndCapitalizeText(string){
     return string.trim().toUpperCase();
+}
+
+function playRound(){
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    if (playerChoice === computerChoice) return "TIE";
+    switch(playerChoice){
+        case "SCISSORS":
+            if (computerChoice === "PAPER") return "PLAYER";
+            if (computerChoice === "ROCK") return "COMPUTER";
+            break;
+        case "PAPER":
+            if (computerChoice === "ROCK") return "PLAYER";
+            if (computerChoice === "SCISSORS") return "COMPUTER";
+            break;
+        case "ROCK":
+            if (computerChoice === "SCISSORS") return "PLAYER";
+            if (computerChoice === "PAPER") return "COMPUTER";
+            break;
+    }
+}
+
+function playGame(){
+    let computerScore = 0;
+    let playerScore = 0;
+    let round = 0;
+
+    while(round < 5){
+        let winner = playRound();
+        if(winner === "PLAYER") ++playerScore;
+        if(winner === "COMPUTER") ++computerScore;
+        ++round;
+    }
+
+    if (playerScore > computerScore)
+        return "Player Wins!";
+    if (playerScore < computerScore)
+        return "Computer Wins!";
+    
+    //Return case of a tie
+    return "It's a Tie!"
 }
